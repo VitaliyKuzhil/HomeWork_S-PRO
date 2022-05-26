25fbc45b75859a097d1908fe54a8ceb5932a9fde
Vitaliy Kuzhil <vitaliy.kuzhil2018@gmail.com>
2022-05-26 23:12:24 +0300

    Adding task descriptions for Home Work W5

--- HomeWork_W5/HW_5_2.py
+++ HomeWork_W5/HW_5_2.py
@@ -8,3 +8,12 @@ print(f'Clear punctuation marks: {clear(char_input)}')
 print(f'Get list: {get_list(char_input)}')
 
 print(f'Get max len char: {max_char(char_input)}')
+
+
+# 2) Реалізуйте модуль word_utils.py, що дозволяє:
+#
+# - очистити пропозицію від розділових знаків;
+#
+# - отримати список слів із пропозиції;
+#
+# - отримати найдовше слово у реченні;
